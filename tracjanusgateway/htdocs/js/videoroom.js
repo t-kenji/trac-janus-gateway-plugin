c26feed6ee77cfc996d23d24f338e291321ccde5
t-kenji <protect.2501@gmail.com>
2018-01-06 00:09:29 +0900

    Change: transport plugin to websocket

--- tracjanusgateway/htdocs/js/videoroom.js
+++ tracjanusgateway/htdocs/js/videoroom.js
@@ -42,11 +42,10 @@
 // in the presented order. The first working server will be used for
 // the whole session.
 //
-var server = null;
-if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
-else
-	server = "https://" + window.location.hostname + ":8089/janus";
+var server = [
+	"wss://" + window.location.hostname + ":8989",
+	"https://" + window.location.hostname + ":8089/janus"
+];
 
 var janus = null;
 var sfutest = null;
